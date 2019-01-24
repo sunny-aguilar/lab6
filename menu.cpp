74141943d82c0f71d7ae1e1290255ea5dd00b336
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-23 18:43:52 -0800

    lab6

---
+++ menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandro on 1/23/2019.
+//
+
+#include "menu.hpp"
---
+++ menu.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sandro on 1/23/2019.
+//
+
+#ifndef LAB6_MENU_HPP
+#define LAB6_MENU_HPP
+
+#endif //LAB6_MENU_HPP
