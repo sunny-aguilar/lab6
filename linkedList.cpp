92f55eb00f0248e8ca1cd97d824019124fe3a4ec
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-23 22:42:41 -0800

    lab3

--- linkedList.cpp
+++ linkedList.cpp
@@ -45,16 +45,22 @@ void LinkedList::menuOption() {
 
     switch (menu.validateNumber(1,6)) {
         case 1:
+
             break;
         case 2:
+
             break;
         case 3:
+
             break;
         case 4:
+
             break;
         case 5:
+
             break;
         case 6:
+
             break;
         default:
             cout << "Unable to make a selection!\n";
