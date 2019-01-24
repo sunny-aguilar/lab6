0b256ab8bb568a8b08b441a1aacfa7a848f545da
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-24 09:22:02 -0800

    lab3

--- node.cpp
+++ node.cpp
@@ -22,33 +22,33 @@ Node::Node() : val{0}, next{nullptr}, prev{nullptr} {}
 ** Description:     adds a new node head to the head
 *********************************************************************/
 void Node::addHead() {
-    cout << ""
+    cout << "adding node to head";
 }
 
 /*********************************************************************
 ** Description:     adds a new node head to the tail
 *********************************************************************/
 void Node::addTail() {
-
+    cout << "adding node to tail";
 }
 
 /*********************************************************************
 ** Description:     deletes the first node in the list
 *********************************************************************/
 void Node::deleteFirstNode() {
-
+    cout << "deleting first node";
 }
 
 /*********************************************************************
 ** Description:     deletes the last node in the list
 *********************************************************************/
 void Node::deleteLastNode() {
-
+    cout << "deleting last node";
 }
 
 /*********************************************************************
 ** Description:     traverse the list reversely
 *********************************************************************/
 void Node::traverseList() {
-
+    cout << "traversing the list in reverse";
 }
