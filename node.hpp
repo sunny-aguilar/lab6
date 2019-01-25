fa8d1d24b2f1dbde56b93b69063ca9f5d72cf95a
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-25 15:02:28 -0800

    lab6

--- node.hpp
+++ node.hpp
@@ -1,17 +1,17 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            Feb 2019
-** Description:     LAB6 - this program allows a user to create a
-**                  simple linked list. A menu first appears that shows
-**                  the user the possible interactions they can have,
-**                  from adding nodes to the head or tail of the list
-**                  to deleting those nodes and printing the the values
-**                  held by each node in reverse. Any time a node is
-**                  updated, the program will print the current node
-**                  list values. A warning will be shown if no nodes
-**                  exist. Recursion is used to print the list in
-**                  reverse. After a user is done interacting with the
-**                  linked-list, they can quit the program.
+** Description:     Node class is the data structure that consists of
+**                  of the node. The node only holds one integer
+**                  data value and two pointers that can hold nodes.
+**                  f
+**                  t
+**                  h
+**                  u
+**                  l
+**                  e
+**                  r
+**                  l
 *********************************************************************/
 
 #ifndef NODE_HPP
