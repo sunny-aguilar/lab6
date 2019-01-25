f7993ae860ac2c012fcf19398a09034efec36486
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-25 15:00:51 -0800

    lab6

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            Feb 2019
-** Description:     LAB6 -
-**                  d
-**                  d
-**                  d
-**                  d
-**                  d
-**                  d
-**                  d
+** Description:     LAB6 - this program allows a user to create a
+**                  simple linked list. A menu first appears that shows
+**                  the user the possible interactions they can have,
+**                  from adding nodes to the head or tail of the list
+**                  to deleting those nodes and printing the the values
+**                  held by each node in reverse. Any time a node is
+**                  updated, the program will print the current node
+**                  list values. A warning will be shown if no nodes
+**                  exist. Recursion is used to print the list in
+**                  reverse. After a user is done interacting with the
+**                  linked-list, they can quit the program.
 *********************************************************************/
 #include "linkedList.hpp"
 #include <iostream>
