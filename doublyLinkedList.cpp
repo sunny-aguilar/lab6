f662f88d32a49a3dda10ecf76f849d767236b85d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-15 08:26:08 -0800

    lab6

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -225,7 +225,7 @@ void DoublyLinkedList::deleteFirstNode() {
 ** Description:     deletes the last node in the list and updates
 **                  tail pointer to last node on list, if it exist.
 **                  First the list is checked to see if there are any
-**                  any nodes in it.
+**                  any nodes in it. If there is, then it checks to 
 *********************************************************************/
 void DoublyLinkedList::deleteLastNode() {
     Node *nodePtr, *prev;
