420ad730483dcfccf46534d73d7fddcfafe891a9
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-15 08:22:32 -0800

    lab6

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -185,7 +185,7 @@ void DoublyLinkedList::addTail(int val) {
 
 /*********************************************************************
 ** Description:     deletes the first node in the list. First it
-**                  checks if the node list
+**                  checks if the node list is empty
 *********************************************************************/
 void DoublyLinkedList::deleteFirstNode() {
     Node *nodePtr;
